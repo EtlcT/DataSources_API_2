eed47c4cf40ba1ffef9f13312bfa2a6a1e60f458
EtlcT <agathe.badetti@epfedu.fr>
2024-01-03 18:10:13 +0100

    add a firestore.py that instantiate firestore client ; add function that automaticaly create a parameters collection if not exists ; add a route to retrieve those parameters from the API.

--- services/epf-flower-data-science/src/api/routes/data.py
+++ services/epf-flower-data-science/src/api/routes/data.py
@@ -3,7 +3,15 @@ from src.schemas.message import MessageResponse
 import kaggle
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 import json
+from joblib import dump, load
+import os
+import sys
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../../"))
+sys.path.append(root_path)
+from firestore import FirestoreClient
+
 
 router = APIRouter()
 
@@ -33,7 +41,7 @@ def process_data():
 @router.get('/split_dataset')
 def train_test_split_iris():
     dataframe_iris = pd.read_csv('services/epf-flower-data-science/src/api/data/irisProcessed.csv')
-    x_train, x_test, y_train, y_test = train_test_split(dataframe_iris.drop('Species', axis=1), dataframe_iris['Species'])
+    x_train, x_test, y_train, y_test = train_test_split(dataframe_iris.drop('Species', axis=1), dataframe_iris['Species'], random_state=42)
     data_json = {
         'train': {
             'features': x_train.to_dict(orient='records'),
@@ -45,3 +53,39 @@ def train_test_split_iris():
         }
     }
     return data_json
+
+@router.get('/train_DecisionTree_iris')
+def train_decisionTree_on_iris():
+    data = train_test_split_iris()
+    x_train = pd.DataFrame(data['train']['features'])
+    y_train = pd.DataFrame(data['train']['labels'])
+    dirname = os.path.dirname(__file__)
+    params_url = os.path.join(dirname, '../../config/model_parameters.json')
+    with open(params_url) as f:
+        params = json.load(f)
+    model = DecisionTreeClassifier(random_state=42, max_depth=params['max_depth'], min_samples_leaf=params['min_samples_leaf'], min_samples_split=params['min_samples_split'])
+    model.fit(x_train,y_train)
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../../../src/models/DecisionTreeClassifier.sav')
+    dump(model, filename)
+    return "DecisionTreeClassifier model's successfully trained on Iris and saved"
+
+@router.get('/iris_prediction_DecisionTreeClassifier')
+def get_predict_iris_DecisionTree():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../../../src/models/DecisionTreeClassifier.sav')
+    data = train_test_split_iris()
+    x_test = pd.DataFrame(data['test']['features'])
+    model = load(filename)
+    y_pred = model.predict(x_test)
+    predictions_json = {
+        'predictions': y_pred.tolist()
+    }
+    return predictions_json
+
+@router.get('/get_Firestore_parameters')
+def get_param_Firestone():
+    client = FirestoreClient()
+    client.exist_or_create_parameters_document()
+    parameters = client.get(collection_name='parameters', document_id='Firestore_param')
+    return parameters
